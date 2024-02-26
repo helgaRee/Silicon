dfe0732291bbeea9ba568b1cf36c93dcdb8fbd8a
Helga Reesalu <helga.reesalu@gmail.com>
2024-02-26 11:32:41 +0100

    signup init

--- Silicon_MVC/Controllers/AuthController.cs
+++ Silicon_MVC/Controllers/AuthController.cs
@@ -4,6 +4,10 @@ namespace Silicon_MVC.Controllers;
 
 public class AuthController : Controller
 {
+    //Genom att ange Route, behöver man inte nå sig up via auth: /auth/aignup
+    //utan det räcker med /signup
+    [Route("/signup")]
+    //IActionResult ger mig tillgång till vyer, parital vyer, statuskoder osv
     public IActionResult SignUp()
     {
         return View();
