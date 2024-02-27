0a1c07336b2c309266a3f655aac0ebad22ac0431
Helga Reesalu <helga.reesalu@gmail.com>
2024-02-27 08:15:42 +0100

    Account init

--- Silicon_MVC/Controllers/AccountController.cs
+++ Silicon_MVC/Controllers/AccountController.cs
@@ -4,5 +4,10 @@ namespace Silicon_MVC.Controllers;
 
 public class AccountController : Controller
 {
+    //private readonly AccountService _accountService;
 
+    //public AccountController(AccountService accountService)
+    //{
+    //    _accountService = accountService;
+    //}
 }
