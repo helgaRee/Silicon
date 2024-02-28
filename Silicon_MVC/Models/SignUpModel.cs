8cb4a0cf2b7140e602428887a780df441588d6ef
Helga Reesalu <helga.reesalu@gmail.com>
2024-02-28 15:21:23 +0100

    signup change

--- Silicon_MVC/Models/SignUpModel.cs
+++ Silicon_MVC/Models/SignUpModel.cs
@@ -6,17 +6,20 @@ namespace Silicon_MVC.Models;
 public class SignUpModel
 
 {
+    [DataType(DataType.Text)]
     [Display(Name = "First name", Prompt = "Enter your first name", Order = 0)]
     [Required(ErrorMessage = "First name is required")]
+    [MinLength(2, ErrorMessage = "Enter a first name")]
     public string Firstname { get; set; } = null!; //tvingande värde
 
 
 
 
 
-
+    [DataType(DataType.Text)]
     [Display(Name = "Last name", Prompt = "Enter your last name", Order = 1)]
     [Required(ErrorMessage = "Last name is required")]
+    [MinLength(2, ErrorMessage = "Enter a last name")]
     public string Lastname { get; set; } = null!;
 
 
@@ -35,8 +38,8 @@ public class SignUpModel
 
 
 
-    [Display(Name = "Password", Prompt = "Enter your password", Order = 3)]
     [DataType(DataType.Password)]
+    [Display(Name = "Password", Prompt = "Enter your password", Order = 3)]
     [Required(ErrorMessage = "Password is required")]
     [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^\\da-zA-Z]).{8,15}$", ErrorMessage = "Invalid Password, must be a strong password.")]
     public string Password { get; set; } = null!;
@@ -44,8 +47,8 @@ public class SignUpModel
 
 
 
-    [Display(Name = "Confirm Password", Prompt = "Confirm your password", Order = 4)]
     [DataType(DataType.Password)]
+    [Display(Name = "Confirm Password", Prompt = "Confirm your password", Order = 4)]
     [Required(ErrorMessage = "Password must be confirmed")]
     [Compare(nameof(Password), ErrorMessage = "Fields doesnt match")]
     public string ConfirmPassword { get; set; } = null!;
